e64482dd1b1fd915a6e6e8c1d40bbde711352084
kirankalsi <kiran11kalsi@gmail.com>
2020-10-29 17:04:15 +0000

    example assessment code and test folder

---
+++ Code/example.py
@@ -0,0 +1,20 @@
+	# <QUESTION 1>
+
+	# Given a string, return the boolean True if it ends in "py", and False if not. Ignore Case.
+
+	# <EXAMPLES>
+
+	# endsDev("ilovepy") → True
+	# endsDev("welovepy") → True
+	# endsDev("welovepyforreal") → False
+	# endsDev("pyiscool") → False
+
+	# <HINT>
+
+	# What was the name of the function we have seen which changes the case of a string?  Use your CLI to access the Python documentation and get help(str).
+    
+def endsPy(input):
+	upper_case_string = input.upper()
+	if upper_case_string[len(upper_case_string) - 2:] == "PY":
+		return True
+	return False
